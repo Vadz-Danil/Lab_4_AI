2d969a2b8b01e7a5373fdc0006aad2f946d0bebc
Vadz-Danil <vadz.danil@gmail.com>
2025-10-02 09:22:03 +0200

    Дороблено завдання №3

--- utilities.py
+++ utilities.py
@@ -1,21 +1,39 @@
-# utilities.py
 import numpy as np
 import matplotlib.pyplot as plt
 
 def visualize_classifier(classifier, X, y):
-    # Определение границ графика
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
+    # Define the minimum and maximum values for X and Y
+    # that will be used in the mesh grid
+    min_x, max_x = X[:, 0].min() - 1.0, X[:, 0].max() + 1.0
+    min_y, max_y = X[:, 1].min() - 1.0, X[:, 1].max() + 1.0
 
-    # Предсказание для каждой точки сетки
-    Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
+    # Define the step size to use in plotting the mesh grid
+    mesh_step_size = 0.01
 
-    # Построение графика
-    plt.contourf(xx, yy, Z, alpha=0.3)
-    plt.scatter(X[:, 0], X[:, 1], c=y, s=50, edgecolor='k')
-    plt.title('Classifier Decision Boundaries')
-    plt.xlabel('Feature 1')
-    plt.ylabel('Feature 2')
+    # Define the mesh grid of X and Y values
+    x_vals, y_vals = np.meshgrid(np.arange(min_x, max_x, mesh_step_size), np.arange(min_y, max_y, mesh_step_size))
+
+    # Run the classifier on the mesh grid
+    output = classifier.predict(np.c_[x_vals.ravel(), y_vals.ravel()])
+
+    # Reshape the output array
+    output = output.reshape(x_vals.shape)
+
+    # Create a plot
+    plt.figure()
+
+    # Choose a color scheme for the plot
+    plt.pcolormesh(x_vals, y_vals, output, cmap=plt.cm.gray)
+
+    # Overlay the training points on the plot
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=75, edgecolors='black', linewidth=1, cmap=plt.cm.Paired)
+
+    # Specify the boundaries of the plot
+    plt.xlim(x_vals.min(), x_vals.max())
+    plt.ylim(y_vals.min(), y_vals.max())
+
+    # Specify the ticks on the X and Y axes
+    plt.xticks((np.arange(int(X[:, 0].min() - 1), int(X[:, 0].max() + 1), 1.0)))
+    plt.yticks((np.arange(int(X[:, 1].min() - 1), int(X[:, 1].max() + 1), 1.0)))
+
     plt.show()
